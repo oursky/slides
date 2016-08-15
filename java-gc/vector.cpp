7f7cf3ca617d9a328474889686839b56254afed6
Johnny Kao <kaojohnny@gmail.com>
2016-08-15 11:36:56 +0800

    Add Java GC slides

---
+++ java-gc/vector.cpp
@@ -0,0 +1,10 @@
+int send_request() {
+    size_t n = read_size();
+    vector<int> elements = vector<int>(n);
+
+    if(read_elements(elements.size(), &elements[0]) < n) {
+        return -1;
+    }
+
+    return 0;
+}
